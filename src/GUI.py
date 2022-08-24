7ea966cef362cfb0cf87bd9f4f7d8d8bd28ea557
Ashutosh Mishra <ashudkl2003@gmail.com>
2022-08-24 23:26:30 +0530

    Update GUI.py

--- src/GUI.py
+++ src/GUI.py
@@ -1,9 +1,11 @@
 import dearpygui.dearpygui as dpg
+import time
 import backend as bk
 
 nums = []
-buffer = 1
-usr_ops = ['+','+','+','+']
+buffer = False
+usr_ops = ['+','+','+','+','+','+','+','+']
+usr_eqn = ""
 
 dpg.create_context()
 dpg.create_viewport(title='Custom Title', width=1280, height=720)
@@ -14,16 +16,16 @@ with dpg.font_registry():
 dpg.set_global_font_scale(1.5)
 
 def render_eqn():
-    eqn_stuff = bk.gen_eqn(3,20,1)
+    eqn_stuff = bk.gen_eqn(2,20,1)
     global nums
     nums = eqn_stuff[1]
     with dpg.window(label="",tag=12,width=800, height=300,pos=[30,60]):
-        with dpg.group(horizontal=True,pos=[250,50]):
+        with dpg.group(horizontal=True,pos=[250,50],tag=13):
             tg = 20
             for i in range(len(nums)-1):
                 dpg.add_text(str(nums[i]))
                 if i < len(nums)-2:
-                    dpg.add_combo(("+","-","*","/"),tag=tg,default_value="+",no_arrow_button=True,width=40,callback=user_eqn)
+                    dpg.add_combo(("+","-","*","/"),tag=tg,default_value=" ",no_arrow_button=True,width=40,callback=user_eqn)
                     tg =tg+1
             equals_str = " = " + str(nums[len(nums)-1])
             dpg.add_text(equals_str)
@@ -31,31 +33,60 @@ def render_eqn():
 
 def user_eqn(sender,value):
     global buffer
-    eqn = ""
+    global usr_eqn
     global usr_ops
+    usr_eqn = ""
     usr_ops[sender-20] = value
     print(sender,value,nums,usr_ops)
     with dpg.window(tag=12,pos=[30,60],width=800,height=300):
         dpg.add_spacer()
-        if buffer > 1:
+        if buffer == True:
             dpg.delete_item(31)
-        buffer += 1
+        buffer = True
         with dpg.group(horizontal=True,pos=[250,100],tag=31):
             for i in range(len(nums)-2):
-                eqn = eqn + str(nums[i]) + str(usr_ops[i])
+                usr_eqn = usr_eqn + str(nums[i]) + str(usr_ops[i])
 
-            eqn += str(nums[len(nums)-2])
-            eqn += ' = ' + str(nums[len(nums)-1])
-            display = 'Your eqn is: ' + eqn
+            usr_eqn += str(nums[len(nums)-2])
+            display_eqn = usr_eqn + ' = ' + str(nums[len(nums)-1])
+            display = 'Your eqn is: ' + display_eqn
             dpg.add_text(display)
+
+def render_buttons():
+    with dpg.window(tag=12,pos=[30,60],width=800,height=300):
+        with dpg.group(horizontal=True,pos=[250,150],tag=32):
+            dpg.add_spacer()
+            show_score = dpg.add_button(label='Exit and show score')
+            skip = dpg.add_button(label='skip')
+            submit = dpg.add_button(label='submit',tag=41,callback=submit_callback)
         
-    
 
-def callback_user_eqn(sender,value):
-    print(sender,value)
+def chck_ans(usr_inp,ans):
+    usr_ans = round(eval(usr_inp),3)
+    if usr_ans == ans:
+        return True
+    else:
+        return False
 
+def submit_callback():
+    global buffer
+    global usr_eqn
+    global nums
+    if chck_ans(usr_eqn,nums[len(nums)-1]):
+        with dpg.window(tag=12):
+            dpg.delete_item(13)
+            dpg.delete_item(31)
+            dpg.add_text("Correct",pos=[30,60])
+            time.sleep(3)
+        dpg.delete_item(12)
+        buffer=False
+        render_eqn()
+        render_buttons()
+            
+        
 def main():
     render_eqn()
+    render_buttons()
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.start_dearpygui()
